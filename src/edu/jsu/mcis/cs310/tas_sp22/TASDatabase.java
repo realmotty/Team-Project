5eeea29bce7dd20608e476b438f5b8790cbab189
JnathanCarter <jcarter20@stu.jsu.edu>
2022-03-04 18:49:43 -0600

    bug fix

--- src/edu/jsu/mcis/cs310/tas_sp22/TASDatabase.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/TASDatabase.java
@@ -123,67 +123,51 @@ public class TASDatabase {
      * @return
      */
     public Employee getEmployee(int employeeId) {
-        ResultSet resultset = null;
-        boolean hasresults;
+        
         Employee result = null;
-        String query = null;
-        PreparedStatement pstSelect = null;
 
         try {
             /* Prepare Select Query */
-            query = "SELECT * FROM employee e WHERE id = ?";
-            pstSelect = connection.prepareStatement(query);
+            String query = "SELECT * FROM employee e WHERE id = ?";
+            PreparedStatement pstSelect = connection.prepareStatement(query);
             pstSelect.setInt(1, employeeId);
 
             /* Execute Select Query */
-            hasresults = pstSelect.execute();
+            boolean hasresults = pstSelect.execute();
 
             /* Check for Results */
             if (hasresults) {
 
                 /* Get Results set */
-                resultset = pstSelect.getResultSet();
-                resultset.next();
+                ResultSet resultset = pstSelect.getResultSet();
+                
+                if (resultset.next()) {
 
-                // map to hold parameters
-                HashMap<String, Object> attributes = new HashMap<String, Object>();
+                    // map to hold parameters
+                    HashMap<String, Object> attributes = new HashMap<>();
 
-                /* get timestamp and convert to LocalDateTime */
-                java.sql.Timestamp activetimestamp = resultset.getTimestamp("active");
-                LocalDateTime activeTime;
-                if (activetimestamp != null) {
+                    /* Populate the attribute map */
+                    attributes.put("id", resultset.getInt("id"));
+                    attributes.put("badgeid", resultset.getString("badgeid"));
+                    attributes.put("firstname", resultset.getString("firstname"));
+                    attributes.put("middlename", resultset.getString("middlename"));
+                    attributes.put("lastname", resultset.getString("lastname"));
+                    attributes.put("employeetypeid", resultset.getInt("employeetypeid"));
+                    attributes.put("departmentid", resultset.getInt("departmentid"));
+                    attributes.put("shiftid", resultset.getInt("shiftid"));
+                    attributes.put("id", String.valueOf(employeeId));
+                    attributes.put("active", resultset.getTimestamp("active").toLocalDateTime());
+                    
+                    java.sql.Timestamp inactive = resultset.getTimestamp("inactive");
+                    
+                    if (inactive != null) {
+                        attributes.put("inactive", inactive.toLocalDateTime());
+                    }
 
-                    activeTime = activetimestamp.toLocalDateTime();
-                } else {
-                    activeTime = null;
+                    /* create Employee object */
+                    result = new Employee(attributes);
+
                 }
-
-                java.sql.Timestamp inactivetimestamp = resultset.getTimestamp("inactive");
-                LocalDateTime inactiveTime;
-                if (inactivetimestamp != null) {
-
-                    inactiveTime = inactivetimestamp.toLocalDateTime();
-                } else {
-                    inactiveTime = null;
-                }
-
-                /* Populate the attribute map */
-                attributes.put("id", resultset.getObject("id"));
-                attributes.put("badgeid", resultset.getObject("badgeid"));
-                attributes.put("firstname", resultset.getObject("firstname"));
-                attributes.put("middlename", resultset.getObject("middlename"));
-                attributes.put("lastname", resultset.getObject("lastname"));
-                attributes.put("employeetypeid", resultset.getObject("employeetypeid"));
-                attributes.put("departmentid", resultset.getObject("departmentid"));
-                attributes.put("shiftid", resultset.getObject("shiftid"));
-                attributes.put("id", resultset.getObject("id"));
-                attributes.put("active", activeTime);
-                attributes.put("inactive", inactiveTime);
-
-                /* create Employee object */
-                result = new Employee(attributes);
-
-                /* Close connections */
 
             }
 
@@ -197,57 +181,29 @@ public class TASDatabase {
 
     // get employee 2
     public Employee getEmployee(Badge badgeId) {
-        ResultSet resultset = null;
-        boolean hasresults;
+        
         Employee result = null;
-        String query = null;
-        PreparedStatement pstSelect = null;
 
         try {
             /* Prepare Select Query */
-            query = "SELECT * FROM employee e WHERE badgeid = ?";
-            pstSelect = connection.prepareStatement(query);
+            String query = "SELECT * FROM employee e WHERE badgeid = ?";
+            PreparedStatement pstSelect = connection.prepareStatement(query);
             pstSelect.setString(1, badgeId.getId());
 
             /* Execute Select Query */
-            hasresults = pstSelect.execute();
+            boolean hasresults = pstSelect.execute();
 
             /* Check for Results */
             if (hasresults) {
 
                 /* Get Results set */
-                resultset = pstSelect.getResultSet();
+                ResultSet resultset = pstSelect.getResultSet();
                 if (resultset.next()) {
                     
+                    int id = resultset.getInt("id");
+                    result = getEmployee(id);
+                    
                 }
-
-                // map to hold parameters
-                HashMap<String, Object> attributes = new HashMap<String, Object>();
-
-                /* get timestamp and convert to LocalDateTime */
-                java.sql.Timestamp activetimestamp = resultset.getTimestamp("active");
-                LocalDateTime activeTime = activetimestamp.toLocalDateTime();
-
-                java.sql.Timestamp inactivetimestamp = resultset.getTimestamp("inactive");
-                LocalDateTime inactiveTime = inactivetimestamp.toLocalDateTime();
-
-                /* Populate the attribute map */
-                attributes.put("id", resultset.getObject("id"));
-                attributes.put("badgeid", resultset.getObject("badgeid"));
-                attributes.put("firstname", resultset.getObject("firstname"));
-                attributes.put("middlename", resultset.getObject("middlename"));
-                attributes.put("lastname", resultset.getObject("lastname"));
-                attributes.put("employeetypeid", resultset.getObject("employeetypeid"));
-                attributes.put("departmentid", resultset.getObject("departmentid"));
-                attributes.put("shiftid", resultset.getObject("shiftid"));
-                attributes.put("id", resultset.getObject("id"));
-                attributes.put("active", activeTime);
-                attributes.put("inactive", inactiveTime);
-
-                /* create Employee object */
-                result = new Employee(attributes);
-
-                /* Close connections */
 
             }
 
