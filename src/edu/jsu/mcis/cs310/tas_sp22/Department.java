9be51ed08b9753cc4c947608baa5c0509b2922c4
JnathanCarter <jcarter20@stu.jsu.edu>
2022-03-15 00:10:38 -0500

    Add constructor and toString to Department

--- src/edu/jsu/mcis/cs310/tas_sp22/Department.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/Department.java
@@ -1,5 +1,25 @@
 package edu.jsu.mcis.cs310.tas_sp22;
 
 public class Department {
-    
+        private final int id;
+        private final String description;
+        private final int terminalId;
+
+        Department(int id, String description, int terminalId) {
+                this.id = id;
+                this.description = description;
+                this.terminalId = terminalId;
+        }
+
+        @Override
+        public String toString() {
+                StringBuilder sb = new StringBuilder();
+
+                sb.append("#").append(id).append(" ");
+                sb.append("(").append(description).append("): ");
+                sb.append("terminalid: ").append(terminalId);
+
+                return sb.toString();
+        }
+
 }
